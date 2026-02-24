59fa123028a0a52135bf0639fa73c0fde71ddacc
IfeanyiPlayzz <isiguzoflorence521@gmail.com>
2026-02-23 19:01:16 -0500

    Delete src directory

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { initEngine } from './core/engine.js';
-import { setupUI } from './ui/sidebar.js';
-
-window.addEventListener('DOMContentLoaded', () => {
-    initEngine();
-    setupUI();
-});
